ee06cd5f5a8b48335307ef7b82ffda91624dc5f0
Asgeir Enersen <asgeir.enersen@inspera.no>
2016-02-24 16:05:14 +0100

    Removed parserConfig

--- react.js
+++ react.js
@@ -2,13 +2,6 @@
 
 module.exports = {
     "extends": "plugin:react/recommended",
-    "parserOptions": {
-        "ecmaVersion": 6,
-            "sourceType": "module",
-            "ecmaFeatures": {
-                "jsx": true
-        }
-    },
     "plugins": ["react"],
     "rules": {
         "react/no-danger": 0
