ae50a679dd70cc0d797dc8a958de128d3665320b
Asgeir Enersen <asgeir.enersen@inspera.no>
2016-02-24 15:46:00 +0100

    Added react.js

---
+++ react.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "ecmaVersion": 6,
+            "sourceType": "module",
+            "ecmaFeatures": {
+                "jsx": true
+        }
+    },
+    "plugins": ["react"]
+};
