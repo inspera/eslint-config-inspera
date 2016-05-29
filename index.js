1aa434bdf9c0993085dc574893b961326dc9d0d2
Asgeir Enersen <asgeir@enersen.com>
2016-05-29 18:43:11 +0200

    Turned some rules from error to warning.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-'use strict';
-
 module.exports = {
     extends: 'airbnb',
 
     rules: {
-        'indent': [2, 4],
-        'comma-dangle': [0]
+      'indent': [2, 4],
+      'comma-dangle': [0],
+      'max-len': [1],
+      'func-names': [1],
+      'prefer-arrow-callback': [1],
+      'quote-props': [1],
+      'space-before-function-paren': [1],
     }
 };
