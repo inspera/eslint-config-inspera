ccb2d21c63709f66255a45bbd4ec60840898efaf
Asgeir Enersen <asgeir.enersen@inspera.no>
2016-08-02 22:47:47 +0200

    ITPL-239 "react/jsx-indent": [2, 2]

--- react.js
+++ react.js
@@ -7,6 +7,6 @@ module.exports = {
         "react/no-danger": 0,
         "react/prefer-es6-class": [0],
         "react/jsx-indent": [2, 4],
-        "react/jsx-indent-props": [2, 4]
+        "react/jsx-indent-props": [2, 2]
     }
 };
