3829175a98f63280e218b37f00fafe6b006a078a
Asgeir Enersen <asgeir.enersen@inspera.no>
2016-08-02 22:26:04 +0200

    ITPL-239 SOme more tweaks

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = {
     extends: 'airbnb',
 
     rules: {
-      'indent': [2, 4],
-      'comma-dangle': [0],
-      'max-len': [1, 140],
-      'func-names': [1],
-      'prefer-arrow-callback': [1],
-      'quote-props': [1],
-      'space-before-function-paren': [1],
+        'indent': [2, 4],
+        'comma-dangle': [0],
+        'max-len': [1, 140],
+        'func-names': [1],
+        'prefer-arrow-callback': [1],
+        'quote-props': [1],
+        'space-before-function-paren': [1],
     }
 };
--- react.js
+++ react.js
@@ -4,6 +4,7 @@ module.exports = {
     "extends": "plugin:react/recommended",
     "plugins": ["react"],
     "rules": {
-        "react/no-danger": 0
+        "react/no-danger": 0,
+        "react/jsx-indent": [2, 4]
     }
 };
