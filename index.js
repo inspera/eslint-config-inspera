7115c9c31e05e7e56662c55d4f6bd6877ab2888f
Asgeir Enersen <asgeir.enersen@inspera.no>
2016-09-06 10:05:20 +0200

    Disabled the linebreak-style to prevent trouble for Windows users.

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ module.exports = {
 
     rules: {
         'arrow-body-style': [0],
-        indent: [2, 4],
-        'max-len': [1, 140],
+        'consistent-return': [2, { treatUndefinedAsUnspecified: false }],
         'func-names': [1],
+        'import/no-extraneous-dependencies': 0,
+        indent: [2, 4],
+        'linebreak-style': 0,
+        'max-len': [1, 140],
         'prefer-arrow-callback': [1],
         'quote-props': [1],
         'space-before-function-paren': [1],
-        'consistent-return': [2, { treatUndefinedAsUnspecified: false }],
-        'import/no-extraneous-dependencies': 0,
     },
 };
