3515e56dc4b9901195622c07cdd4063973ddb733
Asgeir Enersen <asgeir.enersen@inspera.no>
2017-08-25 14:54:10 +0200

    Introducing react/jsx-no-bind, and 'react/require-default-props': 'off'

--- react.js
+++ react.js
@@ -7,10 +7,16 @@ module.exports = {
         'react/jsx-filename-extension': 'off',
         'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
+        'react/jsx-no-bind': ['error', {
+            ignoreRefs: true,
+            allowArrowFunctions: false,
+            allowBind: false,
+        }],
+        'react/jsx-no-undef': ['error', { allowGlobals: true }],
         'react/no-danger': 'off',
         'react/no-multi-comp': 'off',
-        'react/prefer-es6-class': 'off',
         'react/prefer-stateless-function': 'off',
+        'react/require-default-props': 'off',
         'react/sort-comp': ['error', {
             order: [
                 'type-annotations',
